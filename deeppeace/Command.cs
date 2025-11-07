296ac27d1772d6c61eabdcadb65e147da216fe05
Chekov Pavel <asukak@naver.com>
2025-11-07 21:47:32 +0900

    mvvm 패턴을 이용하지만, RelayCommand대신에 Command로 새로작성해보자

---
+++ deeppeace/Command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace deeppeace
+{
+    public class Command : ICommand
+    {
+        //필드 선언 - 델리게이트로 선언
+        private readonly Action<object> _executeMethod;
+        private readonly Func<object, bool> _canexecuteMethod; //null 허용
+        //생성자 선언 - 델리게이트 매개변수로 받음
+        public Command(Action<object> executeMethod, Func<object, bool> canexecuteMethod) //canexecuteMethod는 null 허용
+        {
+            this._executeMethod = executeMethod ?? throw new ArgumentException(nameof(executeMethod));
+            this._canexecuteMethod = canexecuteMethod;
+        }
+        public Command(Action<object> executeMethod) : this(executeMethod, null!) { }
+
+        public bool CanExecute(object? parameter)
+        {
+            //canexecuteMethod가 null이면 true 반환
+            return _canexecuteMethod == null || _canexecuteMethod(parameter!);
+        }
+        public void Execute(object? parameter)
+        {
+            _executeMethod(parameter!); //null 아님을 단언하여 컴파일러에게 전달
+        }
+        public event EventHandler? CanExecuteChanged //CommandManager에 이벤트 등록/해제
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- deeppeace/PeaceModelView.cs
+++ deeppeace/PeaceModelView.cs
@@ -7,8 +7,37 @@ using System.Text;
 using System.Windows.Input;
 using System.Threading.Tasks;
 using deeppeace.PeaceModel;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace deeppeace
 {
-    public class PeaceModelView() { }
+    public class PeaceModelView : INotifyPropertyChanged
+    {
+        //필드 선언 - INotifyPropertyChanged 구현부
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand MyCommand { get; set; }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public PeaceModelView()
+        {
+            MyCommand = new Command(ExecuteMethod, CanExecuteMethod);
+        }
+
+        private bool CanExecuteMethod(object arg)
+        {
+            return true;
+            //throw new NotImplementedException();
+        }
+
+        private void ExecuteMethod(object obj)
+        {
+            MessageBox.Show(Application.Current.MainWindow, "Command ExecuteMethod","deeppeace", MessageBoxButton.OK);
+            //throw new NotImplementedException();
+        }
+    }
+
 }
